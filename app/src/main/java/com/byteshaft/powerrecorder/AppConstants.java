acf47e004a6c0dafa2d8b45bcceb119ab04d10d0
s9iper1 <s9iper1@gmail.com>
2015-09-17 00:15:16 +0500

    change video height

--- app/src/main/java/com/byteshaft/powerrecorder/AppConstants.java
+++ app/src/main/java/com/byteshaft/powerrecorder/AppConstants.java
@@ -3,5 +3,5 @@ package com.byteshaft.powerrecorder;
 public class AppConstants {
 
     public static final int VIDEO_WIDTH = 1280;
-    public static final int VIDEO_HEIGHT = 720;
+    public static final int VIDEO_HEIGHT = 736;
 }
